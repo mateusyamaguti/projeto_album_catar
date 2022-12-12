675b9f17f8b60590062646b9018ee825fbca74e0
artur-baccarin <artur.baccarin@nappsolutions.com>
2022-12-12 08:55:55 -0300

    refactor: add private met and att

--- models.py
+++ models.py
@@ -2,10 +2,10 @@ from random import choice
 
 class Stickers:
     def __init__(self):
-        self.stickers = []
-        self.create_sticker()
+        self.__stickers = []
+        self.__create_sticker()
 
-    def create_countries(self):
+    def __create_countries(self):
         return ['QAT', 'ECU', 'SEN', 'NED',
                 'ENG', 'IRN', 'USA', 'WAL',
                 'ARG', 'KSA', 'MEX', 'POL',
@@ -15,45 +15,54 @@ class Stickers:
                 'BRA', 'SRB', 'SUI', 'CMR',
                 'POR', 'GHA', 'URU', 'COR']
 
-    def create_sticker(self):
-        countries = self.create_countries()
-        self.stickers = ['00', 'FWC1', 'FWC2', 'FWC3', 'FWC4',
+    def __create_sticker(self):
+        countries = self.__create_countries()
+        self.__stickers = ['00', 'FWC1', 'FWC2', 'FWC3', 'FWC4',
                    'FWC5', 'FWC6', 'FWC7', 'FWC8', 'FWC9',
                    'FWC10', 'FWC11', 'FWC12', 'FWC13', 'FWC14',
                    'FWC15', 'FWC16', 'FWC17', 'FWC18']
         for country in countries:
             for cont in range(1, 21):
-                self.stickers.append(f'{country}{cont}')
-        self.stickers += ['FWC19', 'FWC20', 'FWC21', 'FWC22',
-                    'FWC23', 'FWC24', 'FWC25', 'FWC26',
-                    'FWC27', 'FWC28', 'FWC29', 'C1',
-                    'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8']
-        return self.stickers
+                self.__stickers.append(f'{country}{cont}')
+                self.__stickers += ['FWC19', 'FWC20', 'FWC21', 'FWC22',
+                            'FWC23', 'FWC24', 'FWC25', 'FWC26',
+                            'FWC27', 'FWC28', 'FWC29', 'C1',
+                            'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8']
+        return self.__stickers
 
     def open_package(self):
         stickers_in_package = []
         for _ in range(5):
-            stickers_in_package.append(choice(self.stickers))
+            stickers_in_package.append(choice(self.__stickers))
         return stickers_in_package
+
+    def get_all_stickers(self):
+        return self.__stickers
 
 
 class Album:
     def __init__(self):
-        self.stickers_in_album = []
-        self.repeated_stickers = {}
+        self.__stickers_in_album = []
+        self.__repeated_stickers = {}
 
-    def verify_if_i_have_the_sticker(self, stickers):
+    def verify_if_i_have_the_stickers(self, stickers):
         for sticker in stickers:
-            if not sticker in self.stickers_in_album:
-                self.glue_sticker(sticker)
+            if not sticker in self.__stickers_in_album:
+                self.__glue_sticker(sticker)
             else:
-                self.add_in_repeateds(sticker)
+                self.__add_in_repeateds(sticker)
 
-    def glue_sticker(self, sticker):
-        self.stickers_in_album.append(sticker)
+    def __glue_sticker(self, sticker):
+        self.__stickers_in_album.append(sticker)
 
-    def add_in_repeateds(self, sticker):
-        if sticker in self.repeated_stickers.keys():
-            self.repeated_stickers[sticker] += 1
+    def __add_in_repeateds(self, sticker):
+        if sticker in self.__repeated_stickers.keys():
+            self.__repeated_stickers[sticker] += 1
         else:
-            self.repeated_stickers[sticker] = 1
+            self.__repeated_stickers[sticker] = 1
+
+    def get_stickers_in_album(self):
+        return self.__stickers_in_album
+
+    def get_repeated_stickers(self):
+        return self.__repeated_stickers
