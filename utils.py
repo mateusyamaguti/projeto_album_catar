7153e24204e0d1817cbac27e3db749169588eaa5
Camila Fernandes Silva <camila.silva@nappsolutions.com>
2022-12-16 10:33:02 -0300

    feat: criação da função read_csv_file

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import csv
+from os.path import exists
 
 def save_csv_file(csv_name, data_to_save, header):
     with open(csv_name, 'w', encoding='utf8', newline='') as csv_file:
@@ -9,3 +10,11 @@ def save_csv_file(csv_name, data_to_save, header):
                 csv_writer.writerow([data])
             else:
                 csv_writer.writerow(data)
+
+
+def read_csv_file(csv_name):
+    if not exists(csv_name):
+        return
+    with open(csv_name, 'r', encoding='utf8') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        return list(csv_reader)
