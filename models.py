bce31cb5caa46fd981160ccb169975877f3bd635
Camila Fernandes Silva <camila.silva@nappsolutions.com>
2022-12-16 10:32:20 -0300

    feat: criação das funções

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from random import choice
-from utils import save_csv_file
+from utils import save_csv_file, read_csv_file
 
 
 class Stickers:
@@ -19,17 +19,17 @@ class Stickers:
 
     def __create_sticker(self):
         countries = self.__create_countries()
-        self.__stickers = ['00', 'FWC1', 'FWC2', 'FWC3', 'FWC4',
-                   'FWC5', 'FWC6', 'FWC7', 'FWC8', 'FWC9',
-                   'FWC10', 'FWC11', 'FWC12', 'FWC13', 'FWC14',
-                   'FWC15', 'FWC16', 'FWC17', 'FWC18']
+        self.__stickers =   ['00', 'FWC1', 'FWC2', 'FWC3', 'FWC4',
+                            'FWC5', 'FWC6', 'FWC7', 'FWC8', 'FWC9',
+                            'FWC10', 'FWC11', 'FWC12', 'FWC13', 'FWC14',
+                            'FWC15', 'FWC16', 'FWC17', 'FWC18']
         for country in countries:
             for cont in range(1, 21):
                 self.__stickers.append(f'{country}{cont}')
         self.__stickers += ['FWC19', 'FWC20', 'FWC21', 'FWC22',
-                    'FWC23', 'FWC24', 'FWC25', 'FWC26',
-                    'FWC27', 'FWC28', 'FWC29', 'C1',
-                    'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8']
+                            'FWC23', 'FWC24', 'FWC25', 'FWC26',
+                            'FWC27', 'FWC28', 'FWC29', 'C1',
+                            'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8']
 
 
     def open_package(self):
@@ -47,19 +47,6 @@ class Album:
         self.__stickers_in_album = []
         self.__repeated_stickers = {}
 
-    def verify_sticker_in_album(self, sticker):
-        if sticker in self.__stickers_in_album:
-            return True
-        else:
-            return False
-
-    def set_stickers(self, stickers):
-        for sticker in stickers:
-            if not self.verify_sticker_in_album(sticker):
-                self.__glue_sticker(sticker)
-            else:
-                self.__add_in_repeateds(sticker)
-
     def __glue_sticker(self, sticker):
         self.__stickers_in_album.append(sticker)
 
@@ -69,30 +56,57 @@ class Album:
         else:
             self.__repeated_stickers[sticker] = 1
 
+    def verify_sticker_in_album(self, sticker):
+        if sticker in self.__stickers_in_album:
+            return True
+        else:
+            return False
+
+    def verify_sticker_in_repeated_stickers(self, sticker):
+        if self.__repeated_stickers.get(sticker):
+            return True
+        return False
+
+    def set_stickers(self, stickers):
+        for sticker in stickers:
+            if not self.verify_sticker_in_album(sticker):
+                self.__glue_sticker(sticker)
+            else:
+                self.__add_in_repeateds(sticker)
+
     def get_stickers_in_album(self):
         return self.__stickers_in_album
 
     def get_repeated_stickers(self):
         return self.__repeated_stickers
 
+    def get_missing_stickers(self, all_stickers):
+        return [sticker for sticker in all_stickers if sticker not in self.__stickers_in_album]
+
     def create_sticker_in_album_report(self):
         save_csv_file('stickers_in_album.csv', self.__stickers_in_album, ['sticker'])
 
     def create_missing_stickers_in_album_report(self, all_stickers):
-        missing_stickers = [sticker for sticker in all_stickers if sticker not in self.__stickers_in_album]
+        missing_stickers = self.get_missing_stickers(all_stickers)
         save_csv_file('missing_stickers.csv', missing_stickers, ['missing sticker'])
 
     def create_repeated_stickers_report(self):
         save_csv_file('repeated_stickers.csv', self.__repeated_stickers.items(), ['sticker', 'quantity'])
 
+    def read_stickers_in_album_report(self):
+        return read_csv_file('stickers_in_album.csv')
+    
+    def read_missing_stickers_in_album_report(self):
+        return read_csv_file('missing_stickers.csv')
+
+    def read_repeated_stickers_report(self):
+        return read_csv_file('repeated_stickers.csv')
+
     def change_sticker(self, sticker):
-        try:
+        if self.verify_sticker_in_repeated_stickers(sticker):
             self.__repeated_stickers[sticker] -= 1
             if self.__repeated_stickers[sticker] <= 0:
                 del self.__repeated_stickers[sticker]
             return True
-        except KeyError:
+        else:
             return False
-
-# to do:
-# Verificar se precisa ter um atributo das figurinhas faltantes
